5c99e48b9c1f46341cdd7b4cdfbe664f01dba2f7
Use3rnamed <joniruonakangas@gmail.com>
2023-02-26 12:04:38 +0200

    V1.1
    - Player movement
    - Grid system
    - NPCs (TODO AI)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,64 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
+    public static GameManager instance;
+
+    [SerializeField] private float time = 0.1f;
+    [SerializeField] private bool isPlayerTurn = true;
+
+    [SerializeField] private int entityNum = 0;
+    [SerializeField] private List<Entity> entities = new List<Entity>();
+
+    public bool IsPlayerTurn { get => isPlayerTurn; }
 
     private void Awake()
     {
-        if (Instance == null)
+        if (instance == null)
         {
-            Instance = this;
+            instance = this;
         }
         else
         {
             Destroy(gameObject);
         }
+    }
+
+    private void StartTurn()
+    {
+        // TODO: Debug.Log($"{entities[entityNum].name} starts its turn!");
+
+        if (entities[entityNum].GetComponent<PlayerScript>())
+            isPlayerTurn = true;
+        else if (entities[entityNum].IsSentient)
+            Action.SkipAction(entities[entityNum]); // TODO: No AI yet, so skip the turn
+    }
+
+    public void EndTurn()
+    {
+        if (entities[entityNum].GetComponent<PlayerScript>())
+            isPlayerTurn = false;
+
+        if (entityNum == entities.Count - 1)
+            entityNum = 0;
+        else
+            entityNum++;
+
+        StartCoroutine(TurnDelay());
+    }
+
+    private IEnumerator TurnDelay()
+    {
+        yield return new WaitForSeconds(time);
+        StartTurn();
+    }
+
+    public void AddEntity(Entity entity)
+    {
+        entities.Add(entity);
+    }
+
+    public void InsertEntity(Entity entity, int index)
+    {
+        entities.Insert(index, entity);
     }
 }
